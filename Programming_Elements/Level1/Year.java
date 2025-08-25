0344f25fc48eae6ed1cee82aa09865080e2566d6
Aparbal Thakur <aparbalthakur@Aparbals-MacBook-Air.local>
2025-08-25 15:21:57 +0530

    Year.java

---
+++ Programming_Elements/Level1/Year.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Year{
+    public static void main(String []args){
+        Scanner sc=new Scanner (System.in);
+        int birthYear=2020;
+        int currentYear=2024;
+        int CurrentBirth=(currentYear-birthYear);
+        System.out.println(CurrentBirth);
+    }
+}
