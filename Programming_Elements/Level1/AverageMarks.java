a7e6d0e177a397519496aad16c97a46ebe2fd8a3
Aparbal Thakur <aparbalthakur@Aparbals-MacBook-Air.local>
2025-08-25 15:38:43 +0530

    AverageMarks.java

---
+++ Programming_Elements/Level1/AverageMarks.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class AverageMarks{
+    public static void main(String []args){
+        Scanner sc=new Scanner (System.in);
+        double Maths=94;
+        double Physics=95;
+        double Chemistry=96;
+        double AverageMarks=(Maths+Physics+Chemistry)/3;
+        System.out.println(AverageMarks);
+    }
+}
