896fc648295932378dec8dd5984ce55ff7e24cc4
Aparbal singh <aparbal.singh_cs24@gla.ac.in>
2025-08-25 11:12:08 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,5 +1,10 @@
 class sum{
     public static void main(String args[]) {
-        System.out.println("Hello worls");
+        Scanner sc=new Scanner(System.in){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int sum=a+b;
+            System.out.println(sum);
+        }
     }
 }
