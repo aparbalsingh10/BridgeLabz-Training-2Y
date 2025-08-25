57a00564725c6592864cd62853999176013f7722
Aparbal Thakur <aparbalthakur@Aparbals-MacBook-Air.local>
2025-08-25 14:02:55 +0530

    KilometerMiles.java

---
+++ KilometerMiles.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class KilometerMiles{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Double Kilometer=sc.nextDouble();
+        Double Miles=Kilometer*0.621371;
+        System.out.println(Miles);
+    }
+}
