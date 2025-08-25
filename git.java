7bc883d1a611fad7eeb19024dba637138982cc4c
Aparbal Thakur <aparbalthakur@Aparbals-MacBook-Air.local>
2025-08-25 15:06:17 +0530

    Add Readme.md

--- git.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-class firstclass {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-   
-        System.out.println("Welcome to Bridgelabz!");
-     }
-}
