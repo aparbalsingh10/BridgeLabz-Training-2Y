6ce7f19160c0d579abaec8e78ae5f61a60a57c16
Aparbal Thakur <aparbalthakur@Aparbals-MacBook-Air.local>
2025-08-25 23:49:02 +0530

    KilometerToMiles.java

---
+++ Programming_Elements/Level1/KilometerToMiles.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class KilometerToMiles{
+    public static void main(String []args){
+        Scanner sc=new Scanner (System.in);
+        double Distance=sc.nextDouble();
+        double Miles=(Distance*1.6);
+        System.out.println(Miles);
+    }
+}
