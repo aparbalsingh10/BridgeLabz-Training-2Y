fddcf8ae719e1c49cdcf59d06780ca3747597b2f
Aparbal Thakur <aparbalthakur@Aparbals-MacBook-Air.local>
2025-08-25 15:46:46 +0530

    DistanceKilometer.java

---
+++ Programming_Elements/Level1/DistanceKilometer.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class DistanceKilometer{
+    public static void main(String []args){
+        Scanner sc=new Scanner (System.in);
+        double Kilometer=10.8;
+        double Conversion=1.6;
+        double Distance=(Kilometer*Conversion);
+        System.out.println(Distance);
+    }
+}
