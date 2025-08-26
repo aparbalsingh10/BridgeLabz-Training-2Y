29d2af85ab58cb66c1456639c9e3d6d071135b1c
Aparbal Thakur <aparbalthakur@Aparbals-MacBook-Air.local>
2025-08-26 12:11:20 +0530

    SquarePerameter.java

---
+++ Programming_Elements/Level2/SquarePerameter.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class SquarePerameter{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        double  perameter=sc.nextDouble();
+        double  side=perameter/4;
+        System.out.println(side);
+        System.out.println(perameter);
+    }
+}
