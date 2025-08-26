1f7563db86a22befa6079e7363bb4adb10c9eca2
Aparbal Thakur <aparbalthakur@Aparbals-MacBook-Air.local>
2025-08-26 14:07:44 +0530

    DoubleOperators

---
+++ Programming_Elements/Level2/DoubleOperators.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class DoubleOperators{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        double result1= a + b * c;   
+        double result2= a * b + c;   
+        double result3= c + a / b;   
+        double result4= a % b + c;   
+         System.out.println(result1);
+         System.out.println(result2);
+         System.out.println(result3);
+         System.out.println(result4);
+    }
+}
